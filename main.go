abcaef89b7836aabc432e118df5e36383eb76e09
Setiyono Ressly <setiyono.ressly@gmail.com>
2023-07-11 13:54:16 +0700

    add structure folder user

--- main.go
+++ main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"crowdfunding/user"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	dsn := "root:@tcp(127.0.0.1:3306)/crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	// dsn := "root:@tcp(127.0.0.1:3306)/crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	// fmt.Println("koneksi sukses")
+	// var users []user.User
+	// db.Find(&users)
+	// for _, user := range users {
+	// 	fmt.Println(user.Name)
+	// }
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
+	dsn := "root:@tcp(127.0.0.1:3306)/crowdfunding?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+	var users []user.User
+	db.Find(&users)
+	c.JSON(http.StatusOK, users)
 
-	fmt.Println("koneksi sukses")
 }
